ecb749173138307d7c3a4e8283e2e46433e84a10
river4891 <1910700431@mail.sit.edu.cn>
2023-08-17 14:30:53 +0800

    chore: fix init path

--- fish/init.fish
+++ fish/init.fish
@@ -3,9 +3,11 @@
 begin
 	set -l myfilename (status filename)
 	set -l local_dir  (dirname $myfilename)
-	set -l real_dir (cd $local_dir ; pwd)
+	set -l real_dir (cd $local_dir ; echo $PWD)
 
 	for item in $real_dir/conf.d/*
 		source $item
 	end
+	cd $HOME
 end
+
